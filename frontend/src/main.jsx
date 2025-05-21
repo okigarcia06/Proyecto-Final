efcbed30645a41361cf8ecbe649b04b8c85bbab7
Diego Ciriaci Ribone <diegociriaci@hotmail.com>
2025-05-21 18:30:43 -0300

    se creo la estructura del proyecto

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import Header from './Component/Header/Header.jsx'
+import Formulario from './Component/Formulario/Formulario.jsx'
+import Home from './Component/Home/Home.jsx'
+import Footer from './Component/Footer/Footer.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header />
+    <Footer />
+    <App />
+  </StrictMode>,
+)
